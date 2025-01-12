cbb96ccf745809b0399c35ff87e7d1525e73d366
KorawutMing <6432004421@student.chula.ac.th>
2025-01-12 23:02:17 +0700

    fix wrong : symbol

--- transliterate/thai/exceptions.py
+++ transliterate/thai/exceptions.py
@@ -9,13 +9,13 @@ def exceptionWords(word: str) -> str:
         str: Special IPA representation for exception words
     """
     dictionary = {
-        'เฮี๊ยะ': 'hia4',
-        'เซี๊ย': 'si:a4',
+        'เฮี๊ยะ': 'hia4', # ia เอียสั้น
+        'เซี๊ย': 'siːa4', # i:a เอียยาว
         'เชี๊ย': 'cʰiːa4',
-        'เริ่ด': 'rɤt3',
+        'เริ่ด': 'rɤt3', # เริด = rɤːt3 -> เริ่ด = rɤt3
         'กษัตริย์': 'ka2.sat2',
         'ครํ่าหวอด': 'kʰram3.wᴐːt2',
         'เนี่ยะ': 'nia3',
-        'สจ๊วร์ต': 'sa2.cuːat4',
+        'สจ๊วร์ต': 'sa2.cuːat4', # referred from 'สจ๊วด'
     }
     return dictionary.get(word, '')
