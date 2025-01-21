a94d89e099a2fb0b87f6d68e84d11cbd91bdad0e
TitorPs360 <thanat.wongsamut@gmail.com>
2025-01-21 10:44:59 +0700

    Optimize

--- transliterate/thai/core.py
+++ transliterate/thai/core.py
@@ -20,6 +20,7 @@ def convert_text_to_ipa(text: str) -> str:
     def process_regular_syllables(is_last_syllable: bool, is_word_end: bool):
         if regular_syllables:
             combined = ''.join(regular_syllables)
+            # print("Regular syllables:", combined)
             if ipa := transliterate(combined, engine='tltk_ipa'):
                 ipa_segments.append(ipa)
 
@@ -33,19 +34,25 @@ def convert_text_to_ipa(text: str) -> str:
                 # else:
                 #     ipa_segments.append(' ')
             regular_syllables.clear()
+        
+        # print("Current ipa_segments", ipa_segments)
 
     for word in words:
+        # print("word: ", word)
         if word == " ":
+            process_regular_syllables(is_last_syllable=True, is_word_end=True)
+
             if ipa_segments and ipa_segments[-1] != ' ':
                 ipa_segments.append(' ')
             continue
 
         syllables = syllable_tokenize(word)
         for i, syllable in enumerate(syllables):
+            is_first_syllable = i == 0
             is_last_syllable = i == len(syllables) - 1
 
             if exception_ipa := exceptionWords(syllable):
-                process_regular_syllables(is_last_syllable=True, is_word_end=False)
+                process_regular_syllables(is_last_syllable=(not is_last_syllable), is_word_end=is_first_syllable)
                 # ipa_segments.extend([exception_ipa, '.'])
                 ipa_segments.append(exception_ipa)
                 if not is_last_syllable:
